6720ae37f928bcdf2ac48a3e454365962966b1da
Fidele Lesobre <fidelelesobre@gmail.com>
2023-11-17 13:25:57 +0200

    Indewing add new client page

---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,85 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="page-content">
+    <!--breadcrumb-->
+    <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
+        <div class="breadcrumb-title pe-3">Tables</div>
+        <div class="ps-3">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0 p-0">
+                    <li class="breadcrumb-item"><a href="/"><i class="bx bx-home-alt"></i></a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">CLIENTS</li>
+                </ol>
+            </nav>
+        </div>
+        <div class="ms-auto">
+            <div class="btn-group">
+                <a href="{{ route('clients.create') }}" type="button" class="btn btn-primary">NOUVEAU</a>
+            </div>
+        </div>
+    </div>
+    <!--end breadcrumb-->
+
+    <h6 class="mb-0 text-uppercase">CLIENTS</h6>
+    <hr/>
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table id="example2" class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Adresse</th>
+                            <th>Téléphone</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($clients as $client)
+                            <tr>
+                                <td>{{ $client->id }}</td>
+                                <td class="text-uppercase">{{ $client->prenom_client }}</td>
+                                <td class="text-uppercase">{{ $client->nom_client }}</td>
+                                <td class="text-uppercase">{{ $client->adresse_client }}</td>
+                                <td class="text-uppercase">{{ $client->phone_client }}</td>
+                                <td class="text-lowercase">{{ $client->email_client }}</td>
+                                <td>
+                                    <div class="d-flex order-actions">
+                                        <a href="javascript:;" class="text-danger bg-light-danger border-0"><i class="bx bxs-trash"></i></a>
+                                        <a href="javascript:;" class="ms-4 text-primary bg-light-primary border-0"><i class="bx bxs-edit"></i></a>
+                                    </div>
+                                </td>
+                            </tr>
+
+                        @endforeach
+
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th>#</th>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>Adresse</th>
+                            <th>Téléphone</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+
+
+
+
