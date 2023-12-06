0e01ebc14415e58fc49240fa7220e3699e632bbd
Fidele Lesobre <fidelelesobre@gmail.com>
2023-12-06 17:46:56 +0200

    Verifying some bugs on updating product_image

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Produit;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProduitController extends Controller
 {
@@ -18,7 +19,6 @@ class ProduitController extends Controller
         //Charger les données de la DB
         $produits = Produit::all();
         return view('produits.index', compact('produits'));
-
     }
 
     /**
@@ -37,15 +37,13 @@ class ProduitController extends Controller
     {
         //// Validation des champs de formulaire
         // Sachant que validate est inclu dans la classe Request
-        $request->validate
-        ([
+        $request->validate([
             'nom_produit' => 'required|unique:produits,nom_produit',
             'prix' => 'required',
             'date_fabrication_produit' => 'required',
             'date_expiration_produit' => 'required',
             'image_produit' => 'image|mimes:jpg,png,svg,gif|max:2048',
         ]);
-
         // 1: Ajout du client à la base de données
         // 1. 1: Appel de la classe Client étant égal à la classe définie dans Models.Client
         // On stocke le new Client dans une variable pour pas qu'on l'invoque à chaque élement de la Classe
@@ -61,6 +59,19 @@ class ProduitController extends Controller
         $produits->date_expiration_produit = $request->date_expiration_produit;
         $produits->disponibilite = $request->disponibilite;
         // 1. 4: On précise l'action à faire, enregistrer le nouveau client $client = new Client
+        // Traitement ou récupération de l'image
+        $imagepath = null;
+        if ($request->hasFile('image_produit')) {
+            $todayDate = date("YmdHis");
+            // Modi
+            $imagepath = $request->file('image_produit')->storeAs(
+                'images',
+                $todayDate . '.' . $request->file('image_produit')->getClientOriginalExtension(),
+                'public'
+            );
+        }
+        $produits->image_produit = $imagepath;
+
         $produits->save();
 
         // 1. 5: Après l'enregistrement au aura besoin d'être rediriger sur une page pour pas avoir une page blanche
@@ -97,8 +108,7 @@ class ProduitController extends Controller
     {
         // Validation des champs de formulaire
         // Sachant que validate est inclu dans la classe Request
-        $request->validate
-        ([
+        $request->validate([
             'nom_produit' => 'required',
             'prix' => 'required',
             'date_fabrication_produit' => 'required',
@@ -107,6 +117,25 @@ class ProduitController extends Controller
 
         // Identifier d'abord l'élémnt à mettre à jour
         $produits = Produit::findOrfail($id);
+
+        // Vérification si un nouveau fichier image a été téléchargé
+        if ($request->hasFile('image_produit')) {
+            // Supprimer l'ancienne image si elle existe
+            if ($produits->image_produit) {
+                Storage::delete('public/' . $produits->image_produit);
+            }
+            // Enregistrer la nouvelle image
+            $imagepath = null;
+            $todayDate = date("YmdHis");
+            // Modification
+            $imagepath = $request->file('image_produit')->storeAs(
+                'images',
+                $todayDate . '.' . $request->file('image_produit')->getClientOriginalExtension(),
+                'public'
+            );
+            // Mettre à jour le produit avec la nouvelle image
+            $produits->image_produit = $imagepath;
+        }
         // Donnez les valeurs équivalent aux noms des champs sur le formulaire
         $produits->nom_produit = $request->get('nom_produit');
         $produits->prix = $request->get('prix');
@@ -114,9 +143,9 @@ class ProduitController extends Controller
         $produits->date_fabrication_produit = $request->get('date_fabrication_produit');
         $produits->date_expiration_produit = $request->get('date_expiration_produit');
         $produits->disponibilite = $request->get('disponibilite');
+
         // Enregistrez les information de mise à jour
         $produits->save();
-
         // 1. 5: Après l'enregistrement au aura besoin d'être rediriger sur une page pour pas avoir une page blanche
         // !! Ici on nous rediriger vers l'url localhost:post_choisi/clients
         return redirect(route('produits.index'));
